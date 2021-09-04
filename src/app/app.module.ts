2890b723d8911578aab7dd9cccb5869903a4e537
tiennv0608 <tiennv0608@gmail.com>
2021-09-04 14:57:52 +0700

    split part of home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PreloaderStartComponent } from './component/shared/preloader-start/preloader-start.component';
+import { NavbarComponent } from './component/shared/navbar/navbar.component';
+import { FooterComponent } from './component/shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PreloaderStartComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
