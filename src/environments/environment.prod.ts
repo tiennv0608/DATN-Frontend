e3797cde5c6585a70ab71e989ae64b688f069ea5
tiennv0608 <tiennv0608@gmail.com>
2021-09-05 17:15:46 +0700

    chuẩn bị sửa thông tin công ty

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,14 @@
 export const environment = {
   production: true,
-  API_URL: 'http://localhost:8080'
+  API_URL: 'http://localhost:8080',
+  firebaseConfig: {
+    apiKey: 'AIzaSyAGoH0vULX5k64c73EsAp2ud_XSsjOkiAE',
+    authDomain: 'datn-7f8ad.firebaseapp.com',
+    databaseURL: 'https://datn-7f8ad-default-rtdb.firebaseio.com/',
+    projectId: 'datn-7f8ad',
+    storageBucket: 'datn-7f8ad.appspot.com',
+    messagingSenderId: '309876633385',
+    appId: '1:309876633385:web:48a08693f492a67e7a1138',
+    measurementId: 'G-R69286TEWS'
+  }
 };
