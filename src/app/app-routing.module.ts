331c687e642d389a7d5cb7b66524368f718ff244
thaoutpham <dongxuanhien65.dx@gmail.com>
2021-09-05 14:28:04 +0700

    ket noit back end user

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {ListComponent} from "./user/list/list.component";
-import {CreateComponent} from "./user/create/create.component";
-import {EditComponent} from "./user/edit/edit.component";
-import {DeleteComponent} from "./user/delete/delete.component";
-
-// const routes: Routes = [
-//   {
-//     path: 'users/list',
-//     component: ListComponent
-//   }, {
-//     path: 'users/create',
-//     component: CreateComponent
-//   }, {
-//     path: 'users/edit/:id',
-//     component: EditComponent
-//   }, {
-//     path: 'users/delete/:id',
-//     component: DeleteComponent
-//   }
 import {PostListComponent} from './component/post/post-list/post-list.component';
 
 const routes: Routes = [
