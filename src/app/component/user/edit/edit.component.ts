f3cd3f3dce585b8b3bf0ae7dba905d503fdc1cfb
vuongvantu <vuongvantu2001x@gmail.com>
2021-09-13 19:58:55 +0700

    edit-user

--- src/app/component/user/edit/edit.component.ts
+++ src/app/component/user/edit/edit.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {CompanyService} from '../../../service/company.service';
+import {TokenService} from '../../../service/token.service';
+import {Router} from '@angular/router';
+import {UserService} from '../../../service/user.service';
+import {User} from '../../../model/user/user';
 
 @Component({
   selector: 'app-edit',
@@ -7,9 +13,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditComponent implements OnInit {
 
-  constructor() { }
+  infoEditForm: FormGroup = new FormGroup({
+    userName: new FormControl(),
+    phone: new FormControl(),
+  });
+  image? = '';
+  message = '';
+  isPassword = 'password';
+  isConfirmedPassword = 'password';
+  isUpdated = false;
+  user?: User;
+  constructor(private userService: UserService,
+              private tokenService: TokenService,
+              private router: Router) {
+  }
 
   ngOnInit(): void {
+    const id = this.tokenService.getToken().id;
+    this.findById(id);
+  }
+
+  findById(id: string) {
+    return this.userService.findById(id).subscribe(user => {
+      this.infoEditForm = new FormGroup({
+        userName: new FormControl(user.name, [Validators.required]),
+        phone: new FormControl(user.phone, [Validators.required, Validators.pattern(/^\+84\d{9}$/)]),
+      });
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  edit() {
+    const id = this.tokenService.getToken().id;
+    this.userService.findById(id).subscribe(user=>{
+      this.user = user;
+    })
+    const user = {
+      name: this.infoEditForm.value.userName,
+      phone: this.infoEditForm.value.phone
+    };
+    this.userService.update(id, user).subscribe(() => {
+      localStorage.setItem('message', 'Cập nhật thông tin thành công');
+    }, error => {
+      console.log(error);
+    });
+  }
+
+
+  showPass(): void {
+    this.isPassword = (this.isPassword === 'password') ? 'text' : 'password';
+  }
+
+  showConfirmedPass(): void {
+    this.isConfirmedPassword = (this.isConfirmedPassword === 'password') ? 'text' : 'password';
+  }
+
+  reload() {
+    this.router.navigate(['users/account']);
   }
 
 }
